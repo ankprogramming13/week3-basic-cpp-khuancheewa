5098d94c8887ff9bdd961f45873868e56cad138d
Unknown <tremajor415@gmail.com>
2017-11-24 14:06:34 +0700

    ข้อที่1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Miss" << endl;
+  cout << "Khuancheewa Nonpho" << endl;
+  cout << "Amp" << endl;
+  cout << "femal" << endl;
+  cout << "27/10/60" << endl;
+  cout << "workingjob" << endl;
+  cout << "single" << endl;
   return 0;
 }
